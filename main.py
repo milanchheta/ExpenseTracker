4d1fa7ccc9c927e5b6c211537f123d8919d989b9
milanchheta <milanchheta@gmail.com>
2020-12-10 05:27:37 -0500

    test trigger

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def hello_world():
 @app.route('/user',methods = ['POST', 'GET','OPTIONS'])
 def user_endpoint():
     if request.method == 'OPTIONS':
+
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': '*',
