b1e1be3e2f3c2523b16e60af15ad83d5ebc90883
milanchheta <milanchheta@gmail.com>
2020-12-10 22:45:24 -0500

    cleaning and comments

--- main.py
+++ main.py
@@ -1,60 +1,65 @@
-from flask import Flask, request, Response
+'''
+Microservice to handle generation of 
+grpahs for a particular user and expense sheet
+'''
+
+# Import statements
+from flask import Flask, request
 from google.cloud import firestore
-import json
 from flask_cors import CORS, cross_origin
 import pygal
 
+# Config flask app and firestore client
 app = Flask(__name__)
 CORS(app)
-
 db = firestore.Client()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+# /analyse endpoint to handle GET requests
 @app.route('/analyse',methods = ['GET'])
 @cross_origin()
-
 def analyse_endpoint():
-  
-    if request.method=='GET':
-        args=request.args
-        if args:
-            budget=args['budget']
-            expense_sheet_id=args['expense_sheet_id']
-            docs=db.collection(u'cloud_expenses').where(u'expense_sheet_id', u'==', expense_sheet_id).stream()
-            expenses=[]
-            for expense in docs:
-                expenses.append(expense.to_dict())
+    #Generate graph for an expense sheet
+    if request.method == 'GET':
+        try:
 
-            spent=0
-            d={}
-            for expense in expenses:
-                spent+=int(expense["expense_value"])
-                if expense["expense_category"] in d:
-                    d[expense["expense_category"]]+=int(expense["expense_value"])
-                else:
-                    d[expense["expense_category"]]=int(expense["expense_value"])
+            args=request.args
+            if args:
+                budget = args['budget']
+                expense_sheet_id = args['expense_sheet_id']
+                docs = db.collection(u'cloud_expenses').where(u'expense_sheet_id', u'==', expense_sheet_id).stream()
+                expenses = []
+                for expense in docs:
+                    expenses.append(expense.to_dict())
 
-            b_chart = pygal.Bar()
-            b_chart.title = "Overall expenses"
-            b_chart.add("Budget", int(budget))
-            b_chart.add("Spent", spent)
-            barGraph=b_chart.render_data_uri()
+                spent = 0
+                d = {}
+                for expense in expenses:
+                    spent += int(expense["expense_value"])
+                    if expense["expense_category"] in d:
+                        d[expense["expense_category"]] += int(expense["expense_value"])
+                    else:
+                        d[expense["expense_category"]] = int(expense["expense_value"])
 
-            pie_chart = pygal.Pie()
-            pie_chart.title = 'Category wise expenses'
-            for k,v in d.items():
-                pie_chart.add(k,v)
-            pieChart=pie_chart.render_data_uri()
+                b_chart = pygal.Bar()
+                b_chart.title = "Overall expenses"
+                b_chart.add("Budget", int(budget))
+                b_chart.add("Spent", spent)
+                barGraph = b_chart.render_data_uri()
 
-            graphData = {"pieChart":pieChart,"barGraph":barGraph}
-            return (json.dumps(graphData), 200)
-        else:
-            return ('Bad request', 400)
+                pie_chart = pygal.Pie()
+                pie_chart.title = 'Category wise expenses'
+                for k,v in d.items():
+                    pie_chart.add(k,v)
+                pieChart = pie_chart.render_data_uri()
 
+                graphData = {"pieChart":pieChart,"barGraph":barGraph}
+                return (json.dumps(graphData), 200)
+            else:
+                return ('Bad request', 400)
+        except:
+            return ('Internal server error', 503)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    #Run the flask server on port 8080
+    app.run(debug = True, host = '0.0.0.0', port = 8080)
